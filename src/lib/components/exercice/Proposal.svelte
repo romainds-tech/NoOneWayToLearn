0cce311855246a7c79322d4f7c77da217c954a7d
Romain Da Silva <38rom1@gmail.com>
2024-01-10 01:01:26 +0100

    add proposal

---
+++ src/lib/components/exercice/Proposal.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    import {Button, Card} from "flowbite-svelte";
+export let content: string;
+export let state: any;
+
+</script>
+
+{#if typeof state === "undefined"}
+    <Card>
+        <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight">{content}</p>
+        <div class="flex justify-between mt-3">
+            <Button color="red">Refuser</Button>
+            <Button color="green">Valider</Button>
+        </div>
+    </Card>
+{/if}
